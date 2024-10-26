d307a1728020e79c9a61b5d2b20e1e2069452c92
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-10-26 11:37:37 +0530

    Refactor TestcontainersConfig to use DynamicPropertyRegistry

--- src/test/java/com/sivalabs/blog/TestcontainersConfig.java
+++ src/test/java/com/sivalabs/blog/TestcontainersConfig.java
@@ -3,6 +3,7 @@ package com.sivalabs.blog;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -14,6 +15,7 @@ public class TestcontainersConfig {
     @Container
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine");
 
+    @Container
     static GenericContainer<?> mailhog = new GenericContainer<>("mailhog/mailhog:v1.0.1").withExposedPorts(1025);
 
     @Bean
@@ -22,9 +24,10 @@ public class TestcontainersConfig {
         return postgres;
     }
 
-    static {
-        mailhog.start();
-        System.setProperty("spring.mail.host", mailhog.getHost());
-        System.setProperty("spring.mail.port", String.valueOf(mailhog.getMappedPort(1025)));
+    @Bean
+    GenericContainer<?> mailhog(DynamicPropertyRegistry properties) {
+        properties.add("spring.mail.host", mailhog::getHost);
+        properties.add("spring.mail.port", mailhog::getFirstMappedPort);
+        return mailhog;
     }
 }
