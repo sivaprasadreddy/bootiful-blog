888576722d01a6aaf613fde7f149630568318528
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-10-26 10:17:07 +0530

    Disable compose while using Testcontainers to start the application.

--- src/test/java/com/sivalabs/blog/TestBlogApplication.java
+++ src/test/java/com/sivalabs/blog/TestBlogApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestBlogApplication {
 
     public static void main(String[] args) {
+        System.setProperty("spring.docker.compose.enabled", "false");
         SpringApplication.from(BlogApplication::main)
                 .with(TestcontainersConfig.class)
                 .run(args);
