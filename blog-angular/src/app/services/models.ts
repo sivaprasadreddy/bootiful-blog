6f5be55797474e1caadf386228f5c13dcc510707
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-12-11 20:12:52 +0530

    Update angular ui

--- blog-angular/src/app/services/models.ts
+++ blog-angular/src/app/services/models.ts
@@ -16,7 +16,37 @@ export interface CreateUserRequest {
   password: string
   name: string
 }
+
 export interface CreateUserResponse {
   email: string
   name: string
+}
+
+export interface PostsResponse {
+  data: PostUserView[],
+  totalElements: number
+  pageNumber: number
+  totalPages: number
+  isFirst: boolean
+  isLast: boolean
+  hasNext: boolean
+  hasPrevious: boolean
+}
+
+export interface PostUserView {
+  id: number
+  title: string
+  url: string
+  content: string
+  createdBy: User
+  createdAt: Date
+  editable: boolean
+}
+
+
+export interface User {
+  id: number
+  name: string
+  email: string
+  role: string
 }
